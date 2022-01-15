d11066bf436e87b86684a07f8bc8f40fa7097c9f
Somewan <68795802+Somewans@users.noreply.github.com>
2022-01-14 18:21:30 -0800

    Fixed formatting of array

--- binaryConverter.java
+++ binaryConverter.java
@@ -12,9 +12,10 @@ public class binaryConverter {
 
         // Check if there are characters other than 1 and 0 (the array nonBinaryCharArray are characters that need to be searched)
         char[] nonBinaryCharArray = {'2', '3', '4', '5', '6', '7', '8', '9',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        '.', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '`', '~', '\'', '"', '{', '}', '[', ']', '|', '\\', ':', ';', ',', '/', '<', '>'};
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '.', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '`', '~', '\'', '"', '{', '}', '[', ']', '|', '\\', ':',
+            ';', ',', '/', '<', '>'};
 
         for (int y = 0; y < userInput.length(); y++) {
             char integer = userInput.charAt(y);
